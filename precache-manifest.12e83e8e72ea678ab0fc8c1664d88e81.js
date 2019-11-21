627807fc27f8427f1fff418f7ae71e8fd0b66e99
mszalkowski <mac.szalkowski@gmail.com>
2019-11-21 18:29:50 +0100

    Updates

---
+++ precache-manifest.12e83e8e72ea678ab0fc8c1664d88e81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebdf6d2896e18a8b778aedbf4966503",
+    "url": "/users-list/index.html"
+  },
+  {
+    "revision": "ebfacb5b26d567c7deff",
+    "url": "/users-list/static/css/main.2c88d430.chunk.css"
+  },
+  {
+    "revision": "e834eec2a3b6fadd2dfa",
+    "url": "/users-list/static/js/2.d6b126ec.chunk.js"
+  },
+  {
+    "revision": "ebfacb5b26d567c7deff",
+    "url": "/users-list/static/js/main.09c78d34.chunk.js"
+  },
+  {
+    "revision": "73d32d595188ac0ecb00",
+    "url": "/users-list/static/js/runtime-main.613b8280.js"
+  }
+]);
